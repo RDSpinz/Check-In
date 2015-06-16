4f360aa39350710e9817b9fe38b75283b3f40884
RDSpinz <RandSpence@gmail.com>
2015-06-16 13:48:30 -0400

    Initial Commit

---
+++ Check-In/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Check-In
+//
+//  Created by Randall Spence on 6/16/15.
+//  Copyright (c) 2015 Randall Spence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
